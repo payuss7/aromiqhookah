f306dda71d6f4c68fb5111be7cd90b5c113bc9de
Ilya Payusov <ilyapayusov@Noutbuk-Ilya.local>
2025-06-10 13:44:35 +0300

    Исправлена ошибка с mixRoutes.js

--- server/src/routes/mixRoutes.js
+++ server/src/routes/mixRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mixController = require('../controllers/mixController');
 
 // GET /mixes - получить все миксы
-router.get('/', mixController.getAllMixes);
+router.get('/', mixController.getMixes);
 
 // POST /mixes - создать новый микс
 router.post('/', mixController.createMix);
